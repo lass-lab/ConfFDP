28f086256627a75b1d0b2742d17d5b1780194646
gs201 <sg20180546@gmail.com>
2024-09-23 16:21:27 +0900

    secondline

--- femu-scripts/run-fdp.sh
+++ femu-scripts/run-fdp.sh
@@ -93,7 +93,8 @@ sudo $QEMU \
     -device scsi-hd,drive=hd0 \
     -drive file=$OSIMGF,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
     -device femu,devsz_mb=${G80},femu_mode=1 \
-    -device nvme-subsys=on,id=subsys0,fdp=true,fdp.nruh=8,fdp.nrg=32,fdp.runs=40960,driver=nvme \
+    -device nvme-subsys=on,id=subsys0,fdp=true,fdp.nruh=8,fdp.nrg=32,fdp.runs=40960 \
+    -device nvme,id=ctrl0,serial=deadbeef,bus=pcie_root_port0,subsys=subsys0 \
     -net user,hostfwd=tcp::8080-:22 \
     -net nic,model=virtio \
     -nographic \
