0564340dc5bdb14aa60272f71545f250b969b661
gs201 <sg20180546@gmail.com>
2024-11-07 12:11:52 +0900

    script

--- femu-scripts/run-fdp.sh
+++ femu-scripts/run-fdp.sh
@@ -1,30 +1,13 @@
-# Huaicheng Li <hcli@cmu.edu>
+#!/bin/bash
+#
+# Huaicheng Li <huaicheng@vt.edu>
 # Run FEMU as Zoned-Namespace (ZNS) SSDs
 #
 
 # Image directory
-G128=131072
-G96=98304
-G76=77824
-G72=73728
-G68=69632
-G64=65536
-G66=67584
-G67=68608
-G20=20480
-G10=10240
-G5=5120
-
-G72=73728
-
-G80=81920
-IMGDIR=/home/sungjin/images
+IMGDIR=$HOME/images
 # Virtual machine disk image
 OSIMGF=$IMGDIR/fdp.qcow2
-
-# SHARED_DIR=/home/gs201/images/shared/F2FS_WITH_FAR
-
-
 
 if [[ ! -e "$OSIMGF" ]]; then
 	echo ""
@@ -34,64 +17,38 @@ if [[ ! -e "$OSIMGF" ]]; then
 	echo ""
 	exit
 fi
-QEMU=/home/sungjin/ConfFDP/build-femu/qemu-system-x86_64
 
-# sudo $QEMU \
-#     -name "FEMU-FDP-SSD-VM" \
-#     -enable-kvm \
-#     -cpu host \
-#     -smp 64 \
-#     -m 32G \
-#     -device virtio-scsi-pci,id=scsi0 \
-#     -device scsi-hd,drive=hd0 \
-#     -drive file=$OSIMGF,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
-#     -device femu,devsz_mb=${G20},femu_mode=1 \
-#     -device nvme-subsys=on,id=subsys0,fdp=true,fdp.nruh=8,fdp.nrg=32,fdp.runs=40960 \
-#     -device nvme,id=ctrl0,serial=deadbeef,bus=pcie_root_port0,subsys=subsys0 \
-#     -drive id=nvm-1,file=/home/sungjin/images/nvm-1.img,format=raw,if=none,discard=unmap,media=disk,read-only=no \
-#     -device nvme-ns,id=nvm-1,drive=nvme-1,bus=ctrl0,nsid=1,logical_block_size=4096,physical_block_size=4096 \
-#     -net user,hostfwd=tcp::8080-:22 \
-#     -net nic,model=virtio \
-#     -nographic \
-#     -qmp unix:./qmp-sock,server,nowait 2>&1 | tee log
+SSD_SIZE_MB=4096
+NUM_CHANNELS=8
+NUM_CHIPS_PER_CHANNEL=4
+NUM_PLANES_PER_CHIP=2
+NUM_BLOCKS_PER_CHIP=32
+# SLC:1 MLC:2 TLC:3 QLC:4
+# MLC is not allowed
+FLASH_TYPE=4
 
+FEMU_OPTIONS="-device femu"
+FEMU_OPTIONS=${FEMU_OPTIONS}",devsz_mb=${SSD_SIZE_MB}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",namespaces=1"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_ch=${NUM_CHANNELS}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_lun=${NUM_CHIPS_PER_CHANNEL}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_plane=${NUM_PLANES_PER_CHIP}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_blk=${NUM_BLOCKS_PER_CHIP}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",zns_flash_type=${FLASH_TYPE}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",femu_mode=7"
+FEMU_OPTIONS=${FEMU_OPTIONS}",stream_number=4"
 
-# sudo $QEMU \
-#     -name "FEMU-FDP-SSD-VM" \
-#     -enable-kvm \
-#     -cpu host \
-#     -smp 64 \
-#     -m 32G \
-#     -device virtio-scsi-pci,id=scsi0 \
-#     -device scsi-hd,drive=hd0 \
-#     -drive file=$OSIMGF,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
-#     -device nvme-subsys=on,id=subsys0,fdp=true,fdp.nruh=8,fdp.nrg=32,fdp.runs=40960 \
-#     -device femu,devsz_mb=${G20},femu_mode=1 \
-#     -net user,hostfwd=tcp::8080-:22 \
-#     -net nic,model=virtio \
-#     -nographic \
-#     -qmp unix:./qmp-sock,server,nowait 2>&1 | tee log
-
-
-
-sudo $QEMU \
-    -name "FEMU-FDP-SSD-VM" \
+sudo ./qemu-system-x86_64 \
+    -name "FEMU-ZNSSD-VM" \
     -enable-kvm \
     -cpu host \
-    -smp 64 \
-    -m 32G \
+    -smp 4 \
+    -m 4G \
     -device virtio-scsi-pci,id=scsi0 \
     -device scsi-hd,drive=hd0 \
     -drive file=$OSIMGF,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
-    -device pcie-root-port,id=pcie_root_port0,chassis=1,slot=0\
-    -device nvme-subsys=on,id=subsys0,fdp=true,fdp.nruh=8,fdp.nrg=32,fdp.runs=40960 \
-    -device nvme,id=ctrl0,serial=deadbeef,bus=pcie_root_port0,subsys=subsys0 \
-    -drive id=nvm-1,file=/home/sungjin/images/nvm-1.img,format=raw,if=none,discard=unmap,media=disk,read-only=no \
-    -device nvme-ns,id=nvm-1,drive=nvm-1,bus=ctrl0,nsid=1,logical_block_size=4096,physical_block_size=4096 \
+    ${FEMU_OPTIONS} \
     -net user,hostfwd=tcp::8080-:22 \
     -net nic,model=virtio \
     -nographic \
     -qmp unix:./qmp-sock,server,nowait 2>&1 | tee log
-
-
-# -device "nvme-subsys,id=subsys0,fdp=true,fdp.nruh=8,fdp.nrg=32,fdp.runs=40960"
--- femu-scripts/run-ms.sh
+++ femu-scripts/run-ms.sh
@@ -1,13 +1,54 @@
 #!/bin/bash
-#
-# Huaicheng Li <huaicheng@vt.edu>
-# Run FEMU as Zoned-Namespace (ZNS) SSDs
-#
+# Huaicheng Li <huaicheng@cs.uchicago.edu>
+# Run FEMU as a black-box SSD (FTL managed by the device)
 
-# Image directory
+# image directory
 IMGDIR=$HOME/images
 # Virtual machine disk image
-OSIMGF=$IMGDIR/u20s.qcow2
+OSIMGF=$IMGDIR/fdp.qcow2
+
+# Configurable SSD Controller layout parameters (must be power of 2)
+secsz=512 # sector size in bytes
+secs_per_pg=8 # number of sectors in a flash page
+pgs_per_blk=256 # number of pages per flash block
+blks_per_pl=256 # number of blocks per plane
+pls_per_lun=1 # keep it at one, no multiplanes support
+luns_per_ch=8 # number of chips per channel
+nchs=8 # number of channels
+ssd_size=12288 # in megabytes, if you change the above layout parameters, make sure you manually recalculate the ssd size and modify it here, please consider a default 25% overprovisioning ratio.
+
+# Latency in nanoseconds
+pg_rd_lat=40000 # page read latency
+pg_wr_lat=200000 # page write latency
+blk_er_lat=2000000 # block erase latency
+ch_xfer_lat=0 # channel transfer time, ignored for now
+
+# GC Threshold (1-100)
+gc_thres_pcent=75
+gc_thres_pcent_high=95
+
+#-----------------------------------------------------------------------
+
+#Compose the entire FEMU BBSSD command line options
+FEMU_OPTIONS="-device femu"
+FEMU_OPTIONS=${FEMU_OPTIONS}",devsz_mb=${ssd_size}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",namespaces=1"
+FEMU_OPTIONS=${FEMU_OPTIONS}",femu_mode=7"
+FEMU_OPTIONS=${FEMU_OPTIONS}",secsz=${secsz}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",secs_per_pg=${secs_per_pg}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pgs_per_blk=${pgs_per_blk}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",blks_per_pl=${blks_per_pl}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pls_per_lun=${pls_per_lun}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",luns_per_ch=${luns_per_ch}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",nchs=${nchs}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pg_rd_lat=${pg_rd_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",pg_wr_lat=${pg_wr_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",blk_er_lat=${blk_er_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",ch_xfer_lat=${ch_xfer_lat}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",gc_thres_pcent=${gc_thres_pcent}"
+FEMU_OPTIONS=${FEMU_OPTIONS}",gc_thres_pcent_high=${gc_thres_pcent_high}"
+
+echo ${FEMU_OPTIONS}
 
 if [[ ! -e "$OSIMGF" ]]; then
 	echo ""
@@ -18,28 +59,8 @@ if [[ ! -e "$OSIMGF" ]]; then
 	exit
 fi
 
-SSD_SIZE_MB=4096
-NUM_CHANNELS=8
-NUM_CHIPS_PER_CHANNEL=4
-NUM_PLANES_PER_CHIP=2
-NUM_BLOCKS_PER_CHIP=32
-# SLC:1 MLC:2 TLC:3 QLC:4
-# MLC is not allowed
-FLASH_TYPE=4
-
-FEMU_OPTIONS="-device femu"
-FEMU_OPTIONS=${FEMU_OPTIONS}",devsz_mb=${SSD_SIZE_MB}"
-FEMU_OPTIONS=${FEMU_OPTIONS}",namespaces=1"
-FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_ch=${NUM_CHANNELS}"
-FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_lun=${NUM_CHIPS_PER_CHANNEL}"
-FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_plane=${NUM_PLANES_PER_CHIP}"
-FEMU_OPTIONS=${FEMU_OPTIONS}",zns_num_blk=${NUM_BLOCKS_PER_CHIP}"
-FEMU_OPTIONS=${FEMU_OPTIONS}",zns_flash_type=${FLASH_TYPE}"
-FEMU_OPTIONS=${FEMU_OPTIONS}",femu_mode=7"
-FEMU_OPTIONS=${FEMU_OPTIONS}",stream_number=4"
-
 sudo ./qemu-system-x86_64 \
-    -name "FEMU-ZNSSD-VM" \
+    -name "FEMU-BBSSD-VM" \
     -enable-kvm \
     -cpu host \
     -smp 4 \
