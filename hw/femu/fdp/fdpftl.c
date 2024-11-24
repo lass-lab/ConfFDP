533cd1e3a4a7fc58f1c5e4bad77b2627309e249b
gs201 <sg20180546@gmail.com>
2024-11-24 17:16:59 +0900

    dteypdsepc?

--- hw/femu/fdp/fdpftl.c
+++ hw/femu/fdp/fdpftl.c
@@ -1605,7 +1605,7 @@ static uint64_t fdpssd_write(struct ssd *ssd, NvmeRequest *req)
     uint8_t stream_id =dword13.ph;
     uint8_t rg_id =dword13.rg;
     // xnvme_ctx->cmd.nvm.cdw13.dspec = geo.dspec_;  // place_id_
-    printf("sungjin test %d %d dtype %u dspec \n",stream_id,rg_id,dtype,dword13.dspec);
+    printf("sungjin test %d %d dtype %u dspec %u\n",stream_id,rg_id,dtype,dword13.dspec);
     if(stream_id>=ssd->stream_number){
         // printf("sungjin : stream id %u -> %u sizeof %lu\n",stream_id,ssd->stream_number-1,sizeof(NvmeCmdDWORD13));
         printf("sungjin %u/%u -> %u/%u\n",stream_id,rg_id,ssd->stream_number-1,rg_id);
