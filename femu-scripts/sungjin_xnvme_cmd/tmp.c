1f7fabf0a90f70fd72696e592b821d7f2f6e4d37
gs201 <sg20180546@gmail.com>
2025-01-24 20:00:35 +0900

    print all

--- femu-scripts/sungjin_xnvme_cmd/tmp.c
+++ femu-scripts/sungjin_xnvme_cmd/tmp.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <fcntl.h>
+// #include <ioctl.h>
+#include <asm-generic/ioctl.h>
+#define F2FS_IOC_SUNGJIN		_IO(0xf5, 24)
 int main() {
     int pid = 32;
     char buf[256];
 
     sprintf(buf, "%d", pid);
 
-    printf("buf %s, len %lu\n", buf, strlen(buf));
+    printf("buf %s, len %lu %d\n", buf, strlen(buf),F2FS_IOC_SUNGJIN);
 
     // expected result: buf 32, len 2
     return 0;
--- hw/femu/fdp/fdpftl.c
+++ hw/femu/fdp/fdpftl.c
@@ -1649,12 +1649,18 @@ static uint64_t fdpssd_write(struct ssd *ssd, NvmeRequest *req)
     uint16_t pid = le16_to_cpu(rw->dspec);
     uint8_t stream_id = pid & 0xff;
     uint8_t rg_id =pid>>8;
+    
+    uint8_t     dsmgmt=le8_to_cpu(rw->dsmgmt);;
+    uint8_t     rsvd=le8_to_cpu(rw->rsvd)
 #else
     uint16_t rg_id=(req->cmd.nsid-1);
     uint16_t stream_id=0;
 #endif
     // print_sungjin(rg_id);
+    print_sungjin(dsmgmt);
+    print_sungjin(rsvd);
     print_sungjin(pid);
+
     // print_sungjin(req->cmd.nsid);
     // printf("sizeof(NvmeCmd) %lu sizeof(NvmeRwCmd) %lu\n",sizeof(NvmeCmd),sizeof(NvmeRwCmd));
     // xnvme_ctx->cmd.nvm.cdw13.dspec = geo.dspec_;  // place_id_
