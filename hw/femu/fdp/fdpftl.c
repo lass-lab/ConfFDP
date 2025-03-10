36258a1c4ea09bb7f2eeaf58696b9a0270bb080e
gs201 <sg20180546@gmail.com>
2025-03-11 02:50:17 +0900

    nrorg?

--- hw/femu/fdp/fdpftl.c
+++ hw/femu/fdp/fdpftl.c
@@ -1612,7 +1612,7 @@ static uint64_t msssd_trim(struct ssd* ssd,NvmeRequest* req){
     NvmeDsmCmd *dsm = (NvmeDsmCmd *) &req->cmd;
     uint32_t attr = le32_to_cpu(dsm->attributes);
     uint32_t nr_org = (le32_to_cpu(dsm->nr) & 0xff) + 1;
-    uint32_t dw10 = le32_to_cpu(req->cmd->cdw10);
+    uint32_t dw10 = le32_to_cpu(req->cmd.cdw10);
     uint16_t nr = (dw10 & 0xff) + 1;
     if(nr!=nr_org){
         // print_sungjin
