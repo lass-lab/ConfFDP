4fb33da0cd4df2c98d71e67780fe7a0c94f9f3fe
gs201 <sg20180546@gmail.com>
2025-03-12 19:07:12 +0900

    printediscard

--- hw/femu/fdp/fdpftl.c
+++ hw/femu/fdp/fdpftl.c
@@ -1213,10 +1213,10 @@ static int do_gc(struct ssd *ssd, bool force,int rg_id)
     // printf("Current time: %s", asctime(tm_info));
 
     printf("[%s]GC-ing line:%d,ipc=%d,victim=%d,full=%d,free=%d,stream_id=%d,rg_id=%d,start_ch_id=%d,end_ch_id=%d\
-               start_lun_id=%d,end_lun_id=%d block_erased %lu copied %lu\n",asctime(tm_info), ppa.g.blk,
+               start_lun_id=%d,end_lun_id=%d ssd->sungjin_stat.copied %lu block_erased %lu copied %lu\n",asctime(tm_info), ppa.g.blk,
               victim_line->ipc, ssd->lm[rg_id].victim_line_cnt, ssd->lm[rg_id].full_line_cnt,
               ssd->lm[rg_id].free_line_cnt,stream_id,rg_id,start_ch_id,end_ch_id,
-              start_lun_id,end_lun_id, ssd->sungjin_stat.block_erased,ssd->sungjin_stat.copied);
+              start_lun_id,end_lun_id,ssd->sungjin_stat.discard ,ssd->sungjin_stat.block_erased,ssd->sungjin_stat.copied);
     /////////////////////////////////
     to_other_rg=(victim_line->ipc < (ssd->sp.pgs_per_line/10)) ;
 
